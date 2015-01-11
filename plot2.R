2a2f002592f05d16169a027b4c95bde94f4fe008
MichielB79 <mbeckschebe@hotmail.com>
2015-01-11 11:20:51 +0100

    message some cleanup and readme added

--- plot2.R
+++ plot2.R
@@ -1,35 +1,39 @@
-# Part 1: Create a directory 'data' in the working directory, if it doesn't exist, and download & unzip the data
+##### Download and unzip the data in directory 'data' #####
+
 if(!file.exists("data")) {
-  message("Directory 'data' created")
   dir.create("data")
 }
 
 if(!file.exists("data/household_power_consumption.txt")) {
-  message("Download data")
-  
   file_url <- "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip"
   download.file(file_url, destfile="./data/data.zip")
   date.downloaded <- date()
-  list.files("./data")
   unzip("./data/data.zip", exdir="data")
 }
 
-# Part 2: Read table
+
+##### Read the data into R and make a subset #####
+
 data <- read.table("./data/household_power_consumption.txt", header=TRUE, sep=";", na.strings="?")
+data <- data[which(data$Date=="2/2/2007" | data$Date=="1/2/2007"),]
 
-# Part 3: Subset rows and remove data
-subdata <- data[which(data$Date=="2/2/2007" | data$Date=="1/2/2007"),]
-row.names(subdata)<-NULL
-rm("data")
 
-# Part 4: Create Date & Time column
-subdata$DateTime <- paste(subdata$Date, subdata$Time)
-subdata$DateTime <- strptime(subdata$DateTime, format = "%d/%m/%Y %H:%M:%S")
+##### Create Date & Time column #####
 
-# Part 5: Create plot 2
+data$DateTime <- paste(data$Date, data$Time)
+data$DateTime <- strptime(data$DateTime, format = "%d/%m/%Y %H:%M:%S")
+
+
+##### Create plot 2 #####
+
 Sys.setlocale("LC_TIME", "English")
-plot(subdata$DateTime, subdata$Global_active_power, type="l", xlab=NA, ylab="Global Active Power (kilowatts)")
+plot(data$DateTime, data$Global_active_power, 
+     type="l", 
+     xlab=NA, 
+     ylab="Global Active Power (kilowatts)")
 
-# Part 6: Create png file
+
+##### Save plot 2 as png with width 480 and heigt 480 #####
+
 dev.copy(png, file="plot2.png", width=480, height=480)
 dev.off()
